ab73b4fa70ae6747f6e0eb676c26304357b9854a
Md. Shakil <shakil.eub.cse@gmail.com>
2025-10-01 00:47:21 +0600

    Update design

--- stripe_integration/product/urls.py
+++ stripe_integration/product/urls.py
@@ -3,4 +3,7 @@ from product.views import *
 
 urlpatterns = [
     path('',productListView, name='product_list'),
+    path('checkout/<int:product_id>/', CheckoutView, name='checkout'),
+    path('success/',success,name='success'),
+    path('cancel/',cancel,name='cancel'),
 ]
--- stripe_integration/product/views.py
+++ stripe_integration/product/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from product.models import *
+from django.http import JsonResponse
 
 def productListView(request):
     product_data = ProductModel.objects.all()
@@ -8,4 +9,20 @@ def productListView(request):
         "product_data":product_data,
     }
     
-    return render(request, 'products.html', context)
+    return render(request, 'product/products.html', context)
+
+def CheckoutView(request, product_id):
+    product = get_object_or_404(ProductModel, id=product_id)
+    
+    context = {
+        'product': product
+    }
+    return render(request, 'product/checkout.html', context)
+
+def success(request):
+    
+    return JsonResponse({"status":"Success"})
+
+def cancel(request):
+    
+    return JsonResponse({"status":"Cancel"})
