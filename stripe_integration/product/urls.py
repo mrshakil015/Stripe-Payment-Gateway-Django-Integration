646169c1e0a5e61390cdd72d257f4cdfcb7387f9
Md. Shakil <shakil.eub.cse@gmail.com>
2025-10-01 19:12:47 +0600

    Create payment

--- stripe_integration/product/urls.py
+++ stripe_integration/product/urls.py
@@ -4,6 +4,7 @@ from product.views import *
 urlpatterns = [
     path('',productListView, name='product_list'),
     path('checkout/<int:product_id>/', CheckoutView, name='checkout'),
+    path('create-payment/<int:product_id>/', createPaymentView, name='create_payment'),
     path('success/',success,name='success'),
     path('cancel/',cancel,name='cancel'),
 ]
--- stripe_integration/product/views.py
+++ stripe_integration/product/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from product.models import *
 from django.http import JsonResponse
+from django.conf import settings
+import stripe
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 def productListView(request):
     product_data = ProductModel.objects.all()
@@ -26,3 +30,34 @@ def success(request):
 def cancel(request):
     
     return JsonResponse({"status":"Cancel"})
+
+def createPaymentView(request, product_id):
+    YOUR_DOMAIN = "http://127.0.0.1:8000"
+    product = get_object_or_404(ProductModel, id=product_id)
+    order = OrderModel.objects.create(
+        user = request.user,
+        product=product,
+        amount = product.price
+    )
+
+    checkout_session = stripe.checkout.Session.create(
+        line_items=[{
+            'price_data':{
+                'currency':'usd',
+                'unit_amount': int(product.price * 100),
+                'product_data':{
+                    'name': product.name
+                }
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        customer_email = request.user.email,
+        success_url=YOUR_DOMAIN + "/success/",
+        cancel_url=YOUR_DOMAIN + "/cancel/",
+    )
+    
+    order.stripe_checkout_session_id = checkout_session.id
+    order.save()
+    return redirect(checkout_session.url)
+    
